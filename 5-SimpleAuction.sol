43b2cf8fea3bdf4c0d002bdf05708a0efe1f6011
Diego Rubio <darubiomunoz@gmail.com>
2021-02-23 17:57:46 -0300

    Adding comments to the smart contract

--- 5-SimpleAuction.sol
+++ 5-SimpleAuction.sol
@@ -1,31 +1,53 @@
-// SPDX-License-Identifier: GPL-3.0
+-// SPDX-License-Identifier: GPL-3.0-
 
 pragma solidity >=0.7.0 <0.9.0;
 
 contract SimpleAuction {
+    // Parameters of the auction.
+    // Times are either absolute unix timestamp (seconds since 1970-01-01) or time periods in seconds.
     address payable public beneficiary;
     uint public auctionEndTime;
 
+    // Current state of the auction.
     address public highestBidder;
     uint public highestBid;
 
+    // Allowed withdrawls of previous bids.
     mapping(address => uint) pendingReturns;
 
+    // Set to true at the end, disallows any changes.
+    // By default initialized to "false".
     bool ended;
 
+    // Events that will be emitted on changes.
     event HighestBidIncreased(address bidder, uint amount);
     event AuctionEnded(address winner, uint amount);
 
+    // The following is a so-called natspec comment, recognizable by the three slashes.
+    // It will be shown when the user is asked to confirm the transaction.
+
+    /// Create a simple auction with "_biddingTime" seconds bidding time on behalf of the beneficiary address "_beneficiary"
     constructor(uint _biddingTime, address payable _beneficiary) {
         beneficiary = _beneficiary;
         auctionEndTime = block.timestamp + _biddingTime;
     }
 
+    /// Bid on the auction with the value sent together with this transaction.
+    /// The value will only be refunded if the auction is not won.
     function bid() public payable {
+        // No arguments are necessary, all information is already part of the transaction.
+        // The keyword payable is required for the function to be able to receive Ether.
+
+        // Revert the call if the bidding period is over.
         require(block.timestamp <= auctionEndTime, "The auction has already ended.");
+
+        // If the bid is not higher, send the money back
+        // (the failing require will revert all changes in this function execution including it having received the money).
         require(msg.value >= highestBid, "There's already a higher bid. Try bidding higher!");
 
         if(highestBid != 0) {
+            // Sending back the money by simply using highestBidder.send(highestBid) is a security risk because it could execute an untrusted contract.
+            // It is always safer to let the receipients withdraw their money themselves.
             pendingReturns[highestBidder] += highestBid;
         }
 
@@ -34,14 +56,16 @@ contract SimpleAuction {
 
         emit HighestBidIncreased(msg.sender, msg.value);
     }
-
+    /// Withdraw a bid that was overbid.
     function withdraw() public returns(bool) {
         uint amount = pendingReturns[msg.sender];
 
         if(amount > 0) {
+            // It is important to set this to zero because the recipient can call this function again as part of the receiving call before "send" returns.
             pendingReturns[msg.sender] = 0;
 
             if(!payable(msg.sender).send(amount)) {
+                // No need to call throw here, just reset the amount owing.
                 pendingReturns[msg.sender] = amount;
                 return false;
             }
@@ -50,13 +74,19 @@ contract SimpleAuction {
         return true;
     }
 
+    /// End the auction and send the highest bid to the beneficiary. 
     function auctionEnd() public {
+        // It is a good guideline to structure functions that interact
+
+        // Conditions
         require(block.timestamp >= auctionEndTime, "The auction hasn't ended yet.");
         require(!ended, "auctionEnd has already been called.");
 
+        // Effects
         ended = true;
         emit AuctionEnded(highestBidder, highestBid);
 
+        // Interaction
         beneficiary.transfer(highestBid);
     }
 }
